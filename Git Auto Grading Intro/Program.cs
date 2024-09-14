d5a7e91eb981c4c097b15a7204ef063a93547d45
Craig Jensen <craig.jensen1@snow.edu>
2024-09-14 17:44:30 -0600

    Fix

--- Git Auto Grading Intro/Program.cs
+++ Git Auto Grading Intro/Program.cs
@@ -15,18 +15,18 @@
         public static int Add(int a, int b)
         {
             // Code
-            return 0 ; // Placeholder
+            return a + b; // Placeholder
         }
         public static int Subtract(int a, int b)
         {
             // Code
             return 0; // Placeholder
         }
-        public static int Multiply(int a, int b)
-        {
-            // Code
-            return 0; // Placeholder
-        }
+        //public static int Multiply(int a, int b)
+        //{
+        //    // Code
+        //    return 0; // Placeholder
+        //}
         public static decimal Divide(int a, int b)
         {
             // Code
